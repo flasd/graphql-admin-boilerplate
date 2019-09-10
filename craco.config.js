c3ac154b82c8768dd2d803c5ce3f6f1fed3b74b9
Marcel Coelho <flasd2@gmail.com>
2019-09-10 14:23:15 -0300

    feat: setup eslint

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const { whenProd, ESLINT_MODES } = require('@craco/craco');
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+  style: {
+    modules: {
+      localIdentName: whenProd('[hash:base64:8]', '[name]_[local]_[hash:base64:8]'),
+    },
+  },
+  eslint: {
+    mode: ESLINT_MODES.file,
+  },
+
+  plugins: [{
+    plugin: CracoAntDesignPlugin,
+    options: {
+      customizeThemeLessPath: path.join(
+        __dirname,
+        'src/constans/antTheme.less',
+      ),
+    },
+  }],
+};

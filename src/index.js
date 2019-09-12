40a0e674cdc84766249e73b6c473220aeb410e1b
Marcel Coelho <flasd2@gmail.com>
2019-09-12 19:50:28 -0300

    feat: add view units and ignore ant+react warnings

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { setLocalStorageKey } from 'fetch-auth-manager';
 import ptBR from 'antd/es/locale/pt_BR';
+import filterConsole from 'filter-console';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import history from './services/history';
@@ -10,6 +11,7 @@ import { getClient } from './services/apollo';
 import 'nanoreset';
 import '@flasd/focus-fix';
 import '@flasd/focus-fix/styles.css';
+import 'view-units';
 
 setLocalStorageKey('b7181ffb6932f718c818b7f8a55d66a0227ac293');
 
@@ -21,6 +23,11 @@ ReactDOM.render(
   }),
   document.getElementById('root'),
 );
+
+if (process.env.NODE_ENV === 'development') {
+  // Ant uses those functions internally and the warnings are annoying
+  filterConsole('Warning: componentWillReceiveProps has been renamed');
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

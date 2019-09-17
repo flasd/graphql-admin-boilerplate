021510b9dc07daf1d265af8699aa4c7fc7484418
Marcel Coelho <flasd2@gmail.com>
2019-09-17 18:46:39 -0300

    fix: makes withLoading accept container styles

--- src/components/HOC/withLoading/index.js
+++ src/components/HOC/withLoading/index.js
@@ -1,21 +1,28 @@
 import React, { Suspense } from 'react';
 import { compose, withProps } from 'recompose';
 import ContentLoader from 'react-content-loader';
-import withRenderDelay from 'react-delay-render';
+import withRenderDelay from '../delayRender';
 import wrapIn from '../wrapIn';
 
-export function getFallback(Component, delay = 225) {
+export function getFallback(Component, styles, delay) {
   return React.createElement(
-    withRenderDelay({ delay })(ContentLoader),
-    {},
+    withRenderDelay(delay)(ContentLoader),
+    { width: 100, height: 100, style: { ...styles } },
     React.createElement(Component),
   );
 }
 
-export default function withLoading(LoadingComponent) {
+const defaultStyles = { width: 100, height: 100 };
+
+export default function withLoading(LoadingComponent, userStyles, delay = 255) {
+  const styles = {
+    ...defaultStyles,
+    ...userStyles,
+  };
+
   return compose(
     withProps({
-      fallback: getFallback(LoadingComponent),
+      fallback: getFallback(LoadingComponent, styles, delay),
     }),
     wrapIn(Suspense),
   );

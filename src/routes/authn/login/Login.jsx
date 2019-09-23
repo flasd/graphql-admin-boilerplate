ba463ee1219dd7394dfc33c19d2516d24284e873
Marcel Coelho <flasd2@gmail.com>
2019-09-23 20:03:52 -0300

    feat: add socialLogin and emailLogin logic and components

--- src/routes/authn/login/Login.jsx
+++ src/routes/authn/login/Login.jsx
@@ -1,40 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'formik';
-import { Button, Divider } from 'antd';
+import { Link } from 'react-router-dom';
+import {
+  Button, Divider, Typography, Icon,
+} from 'antd';
 import { Helmet } from 'react-helmet';
 import Email from '../../../components/DataEntry/Email';
 import Input from '../../../components/DataEntry/Input';
 import { INPUT_TYPES } from '../../../components/DataEntry/Input/Input';
 import * as styles from './Login.styles';
 import AuthHeader from '../../../components/DataDisplay/AuthHeader';
+import { composePath } from '../../../components/Other/Switcher';
+import authPath from '../A.path';
+import recoverPassword from '../recoverPassword';
+import Spacer from '../../../components/Other/Spacer';
+
+const { Paragraph } = Typography;
 
 export default function Login(props) {
-  const { isValid, submitForm, navigateToSignUp } = props;
+  const {
+    submitForm,
+    navigateToSignUp,
+    emailLoading,
+    googleLoading,
+    facebookLoading,
+    socialLogin,
+  } = props;
 
   return (
     <div>
-      <Helmet title="Login" />
+      <Helmet title="Acesse sua conta" />
 
       <AuthHeader title="Entrar" subTitle="Acesse sua conta" />
 
-      <Field name="email" iconBefore="user" component={Email} label="E-mail" />
+      <Field
+        name="email"
+        iconBefore="user"
+        component={Email}
+        label="E-mail"
+        disabled={emailLoading || facebookLoading || googleLoading}
+      />
       <Field
         name="password"
         component={Input}
         label="Senha"
         type={INPUT_TYPES.PASSOWRD}
         onPressEnter={submitForm}
+        disabled={emailLoading || facebookLoading || googleLoading}
       />
-      <Button block type="primary" disabled={!isValid} onClick={submitForm}>
+      <Spacer space={12} />
+      <Button
+        block
+        type="primary"
+        onClick={submitForm}
+        loading={emailLoading}
+        disabled={facebookLoading || googleLoading}
+      >
         Entrar
       </Button>
 
-      <Divider className={styles.divider}>
-        <span className={styles.dividerText}>Ou</span>
-      </Divider>
+      <Paragraph className={styles.forgotPassword}>
+        <Link to={composePath(recoverPassword.path, authPath)}>
+          Esqueceu seu senha?
+        </Link>
+      </Paragraph>
 
-      <Button block type="link" onClick={navigateToSignUp}>
+      <Divider className={styles.divider} />
+
+      <Button
+        block
+        className={styles.googleButton}
+        loading={googleLoading}
+        onClick={() => socialLogin('google')}
+        disabled={emailLoading || facebookLoading}
+      >
+        <div className={styles.buttonContainer}>
+          <div>
+            <Icon type="google" />
+          </div>
+          <div className={styles.buttonText}>Entrar com o Google</div>
+        </div>
+      </Button>
+
+      <Spacer space={12} />
+
+      <Button
+        block
+        className={styles.facebookButton}
+        loading={facebookLoading}
+        onClick={() => socialLogin('facebook')}
+        disabled={emailLoading || googleLoading}
+      >
+        <div className={styles.buttonContainer}>
+          <div>
+            <Icon type="facebook" theme="filled" />
+          </div>
+          <div className={styles.buttonText}>Entrar com o Facebook</div>
+        </div>
+      </Button>
+
+      <Divider className={styles.divider} />
+
+      <Button block onClick={navigateToSignUp}>
         Criar sua conta
       </Button>
     </div>
@@ -42,7 +110,10 @@ export default function Login(props) {
 }
 
 Login.propTypes = {
-  isValid: PropTypes.bool.isRequired,
   submitForm: PropTypes.func.isRequired,
   navigateToSignUp: PropTypes.func.isRequired,
+  emailLoading: PropTypes.bool.isRequired,
+  googleLoading: PropTypes.bool.isRequired,
+  facebookLoading: PropTypes.bool.isRequired,
+  socialLogin: PropTypes.func.isRequired,
 };
--- src/routes/authn/login/Login.styles.js
+++ src/routes/authn/login/Login.styles.js
@@ -1,4 +1,6 @@
 import { css } from 'linaria';
+import { lighten } from 'polished';
+import { white, facebookBlue, googleBlue } from '../../../constants/colors';
 
 export const subheader = css`
   font-weight: 500;
@@ -8,8 +10,56 @@ export const subheader = css`
   display: block;
 `;
 
+export const buttonContainer = css`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  width: 100%;
+  top: 5px;
+  left: 0;
+  padding-left: 12px;
+`;
+
+export const buttonText = css`
+  flex: 1;
+  text-align: center;
+`;
+
+export const googleButton = css`
+  background-color: ${googleBlue};
+  color: ${white};
+  border-color: transparent;
+  position: relative;
+  text-align: right;
+
+  &:hover, &:focus {
+    background-color: ${lighten(0.1, googleBlue)};
+    color: ${white};
+    border-color: transparent;
+  }
+`;
+
+export const facebookButton = css`
+  background-color: ${facebookBlue};
+  color: ${white};
+  border-color: transparent;
+  position: relative;
+  text-align: right;
+
+  &:hover, &:focus {
+    background-color: ${lighten(0.1, facebookBlue)};
+    color: ${white};
+    border-color: transparent;
+  }
+`;
+
+export const forgotPassword = css`
+  margin-top: 18px;
+  text-align: right;
+`;
+
 export const divider = css`
-  margin: 36px 0 24px!important;
+  margin: 16px 0 16px!important;
 `;
 
 export const dividerText = css`

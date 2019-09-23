aedfbfa67d161fd2d14eca3b289c6e28d22a7520
Marcel Coelho <flasd2@gmail.com>
2019-09-23 20:02:54 -0300

    fix: add mediaQuery to update screen width when not on mobile

--- src/routes/authn/A.styles.js
+++ src/routes/authn/A.styles.js
@@ -7,5 +7,23 @@ export const container = css`
 `;
 
 export const card = css`
-  min-width: 320px;
+  min-width: 300px;
+  max-width: 300px;
+  animation: fadeInRight 200ms ease;
+
+  @media (min-width: 768px) {
+    min-width: 400px;
+  }
+
+  @keyframes fadeInRight {
+    from {
+      opacity: 0;
+      transform: translate3d(20px, 0, 0);
+    }
+
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  }
 `;

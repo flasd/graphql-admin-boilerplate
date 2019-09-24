4745991ff4bc2cf17f78d93e43b754c305a25257
Marcel Coelho <flasd2@gmail.com>
2019-09-24 19:07:29 -0300

    feat: implement create account logic

--- src/routes/authn/createAccount/CreateAccount.jsx
+++ src/routes/authn/createAccount/CreateAccount.jsx
@@ -1,5 +1,92 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'formik';
+import { Helmet } from 'react-helmet';
+import { Button, Divider, Result } from 'antd';
+import { Link } from 'react-router-dom';
+import AuthHeader from '../../../components/DataDisplay/AuthHeader';
+import Input, { INPUT_TYPES } from '../../../components/DataEntry/Input';
+import Email from '../../../components/DataEntry/Email';
+import CheckBox from '../../../components/DataEntry/Checkbox';
+import Spacer from '../../../components/General/Spacer';
 
-export default function CreateAccount() {
-  return <div>Create Account</div>;
+export default function CreateAccount(props) {
+  const {
+    submitForm,
+    navigateToLogin,
+    tosPath,
+    ppPath,
+    accountCreated,
+  } = props;
+
+  const terms = (
+    <span>
+      {'Lí e concordo com os ambos os '}
+      <Link to={tosPath}>Termos de Serviço</Link>
+      {' e '}
+      <Link to={ppPath}>Política de Privacidade</Link>
+      {'.'}
+    </span>
+  );
+
+  if (accountCreated) {
+    return (
+      <>
+        <Helmet title="Conta criada com sucesso! Confira seu e-mail." />
+        <Result
+          status="success"
+          title="Confira seu E-mail"
+          subTitle="Mandamos um e-mail de confirmação para você confirmar sua conta. Basta clicar no link!"
+        />
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <Helmet title="Criar conta - Preencha seus dados" />
+
+      <AuthHeader title="Criar conta" subTitle="Preencha seus dados" />
+
+      <Field component={Input} name="name" label="Nome" autoFocus />
+      <Field component={Email} name="email" label="E-mail" />
+      <Field
+        name="password"
+        component={Input}
+        label="Senha"
+        type={INPUT_TYPES.PASSOWRD}
+      />
+      <Field
+        name="confirmPassword"
+        component={Input}
+        label="Confirmar Senha"
+        type={INPUT_TYPES.PASSOWRD}
+        onPressEnter={submitForm}
+      />
+      <Spacer space={12} />
+      <Field name="tos" component={CheckBox} label={terms} />
+      <Spacer space={4} />
+      <Field
+        name="newsletter"
+        component={CheckBox}
+        label="Gostaria de receber notícias e atualizações."
+      />
+      <Spacer space={12} />
+      <Button type="primary" block onClick={submitForm}>
+        Criar Conta
+      </Button>
+      <Divider />
+      <Button block onClick={navigateToLogin}>
+        Já tem conta? Entre agora
+      </Button>
+    </div>
+  );
 }
+
+CreateAccount.propTypes = {
+  submitForm: PropTypes.func.isRequired,
+  navigateToLogin: PropTypes.func.isRequired,
+  tosPath: PropTypes.string.isRequired,
+  ppPath: PropTypes.string.isRequired,
+  accountCreated: PropTypes.bool.isRequired,
+};
--- src/routes/authn/createAccount/index.js
+++ src/routes/authn/createAccount/index.js
@@ -1,13 +1,91 @@
-import { compose, withProps } from 'recompose';
+import { compose, withProps, withState } from 'recompose';
+import { withFormik } from 'formik';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { message } from 'antd';
 import redirectIfAuthenticated from '../../../components/HOC/redirectIfAuthenticated';
 import renderComponent from '../../../components/HOC/renderComponent';
 import CreateAccount from './CreateAccount';
+import {
+  confirmPasswordRule,
+  emailRule,
+  makeRequired,
+  makeSchema,
+  nameRule,
+  passwordRule,
+  tosRule,
+} from '../../../constants/yup-fields';
+import loginPath from '../login/Login.path';
+import authPath from '../A.path';
+import history from '../../../services/history';
+import { composePath } from '../../../components/Other/Switcher';
+import path from './CreateAccount.path';
+import tosPath from '../tos/TOS.path';
+import ppPath from '../privacyPolicy/PrivacyPolicy.path';
+
+export function privateInjectProps(routeProps, $history) {
+  return {
+    ...routeProps,
+    tosPath: composePath(tosPath, authPath),
+    ppPath: composePath(ppPath, authPath),
+    navigateToLogin: () => $history.push(composePath(loginPath, authPath)),
+  };
+}
+
+export const createAccountMutation = gql`
+  mutation createAccount($input: CreateAccountInput!) {
+    createAccount(input: $input)
+  }
+`;
+
+export const CREATE_ACCOUNT_SCHEMA = makeSchema({
+  name: makeRequired(nameRule),
+  email: makeRequired(emailRule),
+  password: makeRequired(passwordRule),
+  confirmPassword: confirmPasswordRule,
+  tos: makeRequired(tosRule),
+});
+
+export async function privateHandleSubmit(values, formikBag) {
+  const { props, setSubmitting } = formikBag;
+  const { createAccount, setAccountCreated } = props;
+
+  try {
+    await createAccount({
+      variables: {
+        input: {
+          name: values.name,
+          email: values.email,
+          password: values.password,
+        },
+      },
+    });
+
+    setAccountCreated(true);
+  } catch (error) {
+    message.error('CHANGE THIS!!');
+    setSubmitting(false);
+  }
+}
+
+export function privateMapPropsToValues() {
+  return {
+    newsletter: true,
+  };
+}
 
 export default {
-  path: '/create-account',
+  path,
   render: (routeProps) => compose(
     renderComponent,
-    withProps(routeProps),
+    withProps(privateInjectProps(routeProps, history)),
+    withState('accountCreated', 'setAccountCreated', false),
+    graphql(createAccountMutation, { name: 'createAccount' }),
+    withFormik({
+      handleSubmit: privateHandleSubmit,
+      mapPropsToValues: privateMapPropsToValues,
+      validationSchema: CREATE_ACCOUNT_SCHEMA,
+    }),
     redirectIfAuthenticated('/dashboard'),
   )(CreateAccount),
 };

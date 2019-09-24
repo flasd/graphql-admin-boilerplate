6a9b53f1066b5df33188098ebcd1dd64ab395e65
Marcel Coelho <flasd2@gmail.com>
2019-09-24 19:03:41 -0300

    fix: checkbox was not showing error messages

--- src/components/DataEntry/Checkbox/Checkbox.jsx
+++ src/components/DataEntry/Checkbox/Checkbox.jsx
@@ -1,44 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Checkbox as AntCheckbox } from 'antd';
+import { Form, Checkbox as AntCheckbox } from 'antd';
 import {
   inputFieldPropTypes,
   inputFormPropTypes,
 } from '../../../constants/prop-types';
+import * as styles from './Checkbox.styles';
 
-export default function Checkbox(props) {
+const { Item } = Form;
+
+function getValidationStatus(field, form) {
+  const { errors, touched, submitCount } = form;
+  const { name } = field;
+
+  if (touched[name] || submitCount > 0) {
+    return errors[name] ? 'error' : 'success';
+  }
+
+  return '';
+}
+
+function getHelpMessage(field, form) {
+  const { errors, touched, submitCount } = form;
+  const { name } = field;
+
+  if (touched[name] || submitCount > 0) {
+    return errors[name];
+  }
+
+  return '';
+}
+
+export default function Input(props) {
   const {
     id,
     label,
     field,
     form,
+    helpMessage,
     disabled,
   } = props;
 
   const { isSubmitting } = form;
 
   return (
-    <AntCheckbox
-      id={id}
-      name={field.name}
-      onChange={field.onChange}
-      onBlur={field.onBlur}
-      checked={field.value}
-      disabled={disabled || isSubmitting}
-    >
-      {label}
-    </AntCheckbox>
+    <div className={styles.container}>
+      <label htmlFor={id}>
+        <Item
+          validateStatus={getValidationStatus(field, form)}
+          help={getHelpMessage(field, form)}
+          extra={helpMessage}
+        >
+          <AntCheckbox
+            id={id}
+            name={field.name}
+            onChange={field.onChange}
+            onBlur={field.onBlur}
+            checked={field.value}
+            disabled={disabled || isSubmitting}
+          >
+            {label}
+          </AntCheckbox>
+        </Item>
+      </label>
+    </div>
   );
 }
 
-Checkbox.propTypes = {
+Input.propTypes = {
   id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   field: inputFieldPropTypes.isRequired,
   form: inputFormPropTypes.isRequired,
+  helpMessage: PropTypes.string,
   disabled: PropTypes.bool,
 };
 
-Checkbox.defaultProps = {
+Input.defaultProps = {
+  helpMessage: '',
   disabled: false,
 };
